b845fb3da7a43b91178a5263c3b49692c1202eef
Matteo Morlacchi <matteo.morlack@gmail.com>
2024-05-06 23:15:57 +0200

    add

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,13 @@
 import * as vscode from "vscode";
-import * as fs from "fs";
-import * as path from "path";
-import dirTree from "directory-tree";
+import * as FS from "fs";
+import * as PATH from "path";
+import dirTree from 'directory-tree';
 import { DirectoryTree, DirectoryTreeOptions, DirectoryTreeCallback } from 'directory-tree';
 
 
 // This method is called when your extension is activated
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Extension project-browser active');
+  console.log("Extension project-browser active");
   let disposable = vscode.commands.registerCommand("test.helloWorld", () => {
     vscode.window.showInformationMessage("Hello World from test!");
   });
@@ -20,54 +20,39 @@ export function activate(context: vscode.ExtensionContext) {
 // This method is called when your extension is deactivated
 export function deactivate() {}
 
-export class ProjectsDataProvider implements vscode.TreeDataProvider<Repository> {
+export class ProjectsDataProvider implements vscode.TreeDataProvider<NodeItem> {
   constructor(private projectsRoots: string) {
     this.projectsRoots = projectsRoots;
   }
 
-  getTreeItem(element: Repository): vscode.TreeItem {
+  getTreeItem(element: NodeItem): vscode.TreeItem {
     return element;
   }
 
-  getChildren(element?: Repository): Thenable<Repository[]> {
+  getChildren(element?: NodeItem): Thenable<NodeItem[]> {
     if (element) {
-      var folderList = this.getDirectoryFolders(element.location);
-      return Promise.resolve(folderList);
+      return Promise.resolve(element.children);
     } else {
       console.log(this.projectsRoots);
-      if (fs.existsSync(this.projectsRoots)) {
-        //const filteredTree = dirTree("/", { extensions: /.*/, depth: 2 });
-        var folderList = this.getDirectoryFolders(this.projectsRoots);
-        return Promise.resolve(folderList);
+      if (FS.existsSync(this.projectsRoots)) {
+        let nodeTree = readDirData(this.projectsRoots, 3);
+        //var folderList = this.getDirectoryFolders(this.projectsRoots);
+        return Promise.resolve([nodeTree]);
       } else {
         return Promise.resolve([]);
       }
     }
   }
 
-  getDirectoryFolders(baseFolder: string): Repository[] {
-    var folderList: Repository[] = [];
-    const options = {
-      depth: 1
-    };
-    fs.readdirSync(baseFolder, {
-      recursive: false,
-      withFileTypes: true,
-    })
-      .filter((dirent) => dirent.isDirectory())
-      .map((dirent) =>
-        folderList.push(new Repository(dirent.name, path.join(dirent.path, dirent.name), vscode.TreeItemCollapsibleState.Expanded))
-      );
-    return folderList;
-  }
 }
 
-class Repository extends vscode.TreeItem {
+class NodeItem extends vscode.TreeItem {
   private isFolder: boolean = true;
   constructor(
     public readonly label: string,
     public location: string,
-    public readonly collapsibleState: vscode.TreeItemCollapsibleState
+    public readonly collapsibleState: vscode.TreeItemCollapsibleState = vscode.TreeItemCollapsibleState.Expanded,
+    public children: NodeItem[] = [],
   ) {
     super(label, collapsibleState);
     this.tooltip = `${this.location}`;
@@ -75,4 +60,29 @@ class Repository extends vscode.TreeItem {
   }
 
   iconPath = new vscode.ThemeIcon(this.isFolder ? "folder" : "repo");
+}
+
+
+function readDirData(path: string, maxDepth: number, currentDepth: number = 0): NodeItem {
+  let item = new NodeItem(PATH.basename(path), path);
+  let dirData = safeReadDirSync(path);
+  if (maxDepth > currentDepth + 1) {
+    item.children = dirData.map(child => readDirData(PATH.join(child.path, child.name), maxDepth, currentDepth + 1));
+  }
+  return item;
+}
+
+function safeReadDirSync(path: string): FS.Dirent[] {
+  try {
+    let dirData = FS.readdirSync(path, {withFileTypes: true}).filter(i => i.isDirectory());
+    return dirData;
+  } catch(ex: any) {
+    if (ex.code == "EACCES" || ex.code == "EPERM") {
+      //User does not have permissions, ignore directory
+      return [];
+    }
+    else {
+      throw ex;
+    }
+  }
 }
